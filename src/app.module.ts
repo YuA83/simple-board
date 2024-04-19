c2410388b905b9b98be97385e8f1ce8aae30454d
유경아 <kayoo@eluocnc.com>
2024-04-19 16:42:58 +0900

    add: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { validationSchema } from './config/validationSchema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
