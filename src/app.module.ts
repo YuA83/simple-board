58cf07ff7df62b1b96d934f40c3ab55e5781db81
유경아 <kayoo@eluocnc.com>
2024-04-19 16:48:55 +0900

    add: typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { validationSchema } from './config/validationSchema';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { validationSchema } from "./config/validationSchema";
 
 @Module({
   imports: [
@@ -10,6 +11,16 @@ import { validationSchema } from './config/validationSchema';
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE,
+      synchronize: process.env.DATABASE_SYNCHRONUZE === "true",
+      entities: [`${__dirname}/**/*.entity.{ts,js}`],
     }),
   ],
   controllers: [AppController],
